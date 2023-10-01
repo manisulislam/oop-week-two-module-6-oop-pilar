6a7c3aee50f858bdfac52adc0784b221ef3ee8c9
anisul islam <anisulislam980340@gmail.com>
2023-10-01 23:35:36 +0600

    inheritance

--- inheritance.py
+++ inheritance.py
@@ -9,7 +9,7 @@ class Device:
         print(f"running laptop : {self.brand}")
 
 class Laptop:
-    def __init__(self, brand, price, color, memory):
+    def __init__(self,memory):
        
         self.memory=memory
     
@@ -17,18 +17,23 @@ class Laptop:
         return f"Learning python and practicing"
 
 class Phone:
-    def __init__(self, brand, price, color, dual_sim):
+    def __init__(self,dual_sim):
         
         self.dual_sim= dual_sim
     
     
     def phone_call(self, number,sms):
         return f"phone number : {number} with {sms}"
+    def __repr__(self) -> str:
+        return f"phone : {self.dual_sim}"
 
 class Camera:
-    def __init__(self, brand, price, color, pixel):
+    def __init__(self, pixel):
        
         self.pixel= pixel
    
     def change_lens(self):
         pass
+
+my_phone= Phone(True)
+print(my_phone)
