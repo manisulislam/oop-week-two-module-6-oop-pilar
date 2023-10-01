a9914c42d9051c6e4e27025184e78e3ad5764e02
anisul islam <anisulislam980340@gmail.com>
2023-10-01 23:25:46 +0600

    inheritance

---
+++ inheritance.py
@@ -0,0 +1,43 @@
+
+
+class Device:
+    def __init__(self, brand, price, color):
+        self.brand=brand
+        self.price=price
+        self.color=color
+    def run(self):
+        print(f"running laptop : {self.brand}")
+
+class Laptop:
+    def __init__(self, brand, price, color, memory):
+        self.brand=brand
+        self.price=price
+        self.color=color
+        self.memory=memory
+    def run(self):
+        print(f"running laptop : {self.brand}")
+    def coding(self):
+        return f"Learning python and practicing"
+
+class Phone:
+    def __init__(self, brand, price, color, dual_sim):
+        self.brand= brand
+        self.price= price
+        self.color=color
+        self.dual_sim= dual_sim
+    
+    def run(self):
+        return f"phone tipa tipi "
+    def phone_call(self, number,sms):
+        return f"phone number : {number} with {sms}"
+
+class Camera:
+    def __init__(self, brand, price, color, pixel):
+        self.brand= brand
+        self.price= price
+        self.color= color
+        self.pixel= pixel
+    def run(self):
+        pass
+    def change_lens(self):
+        pass
